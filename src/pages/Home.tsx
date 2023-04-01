9cb7dae512ae92e539801cb7b5ab13fe5ac8d9a0
Isaac Almeida <isaacalmeidafilho@gmail.com>
2023-04-01 13:54:34 -0300

    comentários

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,3 +7,13 @@ export function Home() {
     </div>
   );
 }
+
+
+// search por nome do produto
+// - tela de detalhes do produtos -> Página de detalhes do produtos
+// - filtro por categoria (ex: frios, laticínios) -> SideBar com as categoria
+// - ordenação (ex: menor preço, maior preço, nome) -> Botão de filtro/Popover?
+// - botão de adicionar e remover do carrinho de compras // dentro do card
+// - carrinho de compras (não é necessário implementar checkout) // pagina
+//   apenas valor total no carrinho // checkout com total no carrinho
+// Backend com Node publicado:
